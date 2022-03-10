9d1b7512fa2289eca72d064392e77a1b356559f5
Kunal Kanotra <kanotrakunal030501@gmail.com>
2022-03-10 15:29:55 +0530

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,91 @@
+import React , {useEffect,useState} from 'react';
+import { CssBaseline , Grid} from "@material-ui/core";
+
+import Header from './components/Header/Header';
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+
+import {getPlacesData , getWeatherData} from "./api";
+
+const App = () => {
+    const [places,setPlaces] = useState([]);
+    const [filteredPlaces , setFilteredPlaces] = useState([]);
+    const [weatherData,setWeatherData] = useState(null);
+
+    const [coordinates,setCoordinates] = useState({});
+
+    const [currentCoordinates,setCurrentCoordinates] = useState({});
+
+    const [bounds,setBounds] = useState(null);
+
+    const [childClicked , setChildClicked] = useState(null);
+
+    const [isLoading , setIsLoading] = useState(false);
+
+    const [type , setType] = useState("restaurants");
+    const [rating , setRating] = useState("");
+
+    useEffect(()=> {
+        navigator.geolocation.getCurrentPosition(({coords:{latitude,longitude}})=>{
+            setCoordinates({lat : latitude , lng : longitude});
+
+            setCurrentCoordinates({lat : latitude , lng : longitude});
+        });
+    },[]);
+
+    useEffect(() => {
+       const  filtered = places.filter((place) => place.rating>rating);
+        setFilteredPlaces(filtered);
+    },[rating]);
+
+    useEffect(()=>{
+            if(bounds)
+            {   
+                setIsLoading(true)
+
+                getWeatherData(coordinates.lat , coordinates.lng).then((data) => {
+                    setWeatherData(data);
+                });
+
+                getPlacesData(type , bounds.sw,bounds.ne).then((data)=>{
+                setPlaces(data?.filter((place) => place.name && place.num_reviews>0));
+
+                setFilteredPlaces([]);
+                setIsLoading(false);
+            });
+            }
+    },[type , bounds]);
+
+    return (
+        <>
+            <CssBaseline/>
+            <Header setCoordinates={setCoordinates} />
+            <Grid container spacing={3} style={{width : "100%"}} >
+                <Grid item xs={12} md={4}>
+                    <List 
+                        places={filteredPlaces.length ? filteredPlaces : places}
+                        childClicked={childClicked}
+                        isLoading={isLoading}
+                        type={type}
+                        setType={setType}
+                        rating={rating}
+                        setRating={setRating}
+                    />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map
+                        setCoordinates={setCoordinates}
+                        setBounds = {setBounds}
+                        coordinates = {coordinates}
+                        currentCoordinates={currentCoordinates}
+                        places={filteredPlaces.length ? filteredPlaces : places}
+                        setChildClicked={setChildClicked}
+                        weatherData={weatherData}
+                    />
+                </Grid>
+            </Grid>
+        </>
+    )
+}
+
+export default App;
